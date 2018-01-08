54ef57feaac32238266f4adafeac38e2e1021933
e.jager@amsterdam.nl <info@lytrix.com>
2018-01-08 14:37:37 +0100

    fixed phpsessid retrieval

--- crowdownloader/downloadRounds.py
+++ crowdownloader/downloadRounds.py
@@ -10,91 +10,85 @@ import parser
 import argparse
 
 
-def get_login_cookies(pastedPHPSESSID):
-    """ 
+def get_login_cookies(session, baseUrl):
+    """
         Get PHPSESSID cookie to use the API
     """
     credentials = config('loginCredentials')
 
-    login_url = "https://amsterdam.apptimizeplatform.nl/login"
-    s = requests.session()
-    r = s.get(login_url)
-    # print(r.cookies)
-
-    loginPage = r.text
-    soup = BeautifulSoup(loginPage, "html.parser")
+    print("start login")
+    loginPage = session.get(baseUrl + '/login')
+    soup = BeautifulSoup(loginPage.text, "html.parser")
     csrf = soup.find("input", type="hidden")
-    csrf_token = csrf['value']
-
-    # print('crsf=' + csrf_token)
+    print(csrf)
+    # Get user and password from config.ini file
+    credentials = config('loginCredentials')
 
     payload = {'_username': credentials['user'],
                '_password': credentials['password'],
-               '_csrf_token': csrf_token,
+               '_csrf_token': csrf['value'],
                }
 
-    r = s.post(login_url, data=payload)
-    print(r.cookies)
-    #print(r.headers)
-    
-    headers = {
-        'User-Agent': 'Mozilla/5.0',
-        'Host': 'amsterdam.apptimizeplatform.nl',
-        'Accept': 'application/json, text/plain, */*',
-        'Accept-Encoding': 'gzip, deflate, br',
-        'Referer': 'https://amsterdam.apptimizeplatform.nl/schouwronde/rapportage/data_export',
-        'Cookie': 'PHPSESSID='+ pastedPHPSESSID + ';',
-        'Connection': "keep-alive"}
-
-    test = requests.get('https://amsterdam.apptimizeplatform.nl/api/inspectionround/area/rounds/planning', headers=headers)
-    if test.status_code == 200:
-        print('login succeeded!')
-        return headers
-
-    if test.status_code == 401:
-        print('login failed! Insert PHPSESSID number from Cookie page from your webbrowser after you login')
+    # Fake browser header
+    headers = {'user-agent': "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.98 Safari/537.36"}
+    loginCheck = session.post(baseUrl + '/login_check',
+                              data=payload,
+                              headers=headers,
+                              cookies=loginPage.cookies)
+   # Response ok or not?
+    if loginCheck.status_code == 200:
+        securePage = session.get('https://amsterdam.apptimizeplatform.nl/api/inspectionround/area/rounds/planning', headers=headers)
+        # Check for name in html page which is only visible after login
+        if securePage.status_code == 200:
+            print("login succeeded!")
+            return loginCheck.cookies
+    if loginCheck.status_code == 401 or loginCheck == 403:
+        print('login failed!')
 
 
-def getListofRounds(baseApiUrl, headers):
+def getListofRounds(session, baseApiUrl, cookies):
     roundsIds = []
     apiUrls = ['area', 'object']
 
     for apiUrl in apiUrls:
 
-        rounds = requests.get(baseApiUrl + '/' + apiUrl + '/rounds/planning', headers=headers)
+        rounds = session.get(baseApiUrl + '/' + apiUrl + '/rounds/planning', cookies=cookies)
         roundsJson = rounds.json()
 
 
-        roundsItems = [{'id': r['id'], 'name': r['name'].replace('/','-'), 'type': apiUrl} for r in roundsJson]
+        roundsItems = [{'id': r['id'], 'name': r['name'].replace('/', '-'), 'type': apiUrl} for r in roundsJson]
         roundsIds.extend(roundsItems)
     print(roundsIds)
     return roundsIds
 
 
-def getXLSofRounds(roundsIds, baseApiUrl, headers):
+def getXLSofRounds(session, roundsIds, baseApiUrl, cookies, resultsFolder):
     for roundsId in roundsIds:
         apiUrl = baseApiUrl + '/' + roundsId['type'] + '/rounds/' + str(roundsId['id']) + '/report/results/excel'
         print(apiUrl)
-        resp = requests.get(apiUrl, headers=headers, stream=True)
+        resp = session.get(apiUrl, cookies=cookies, stream=True)
         resp.raw.decode_content = True
 
         if resp.status_code == 200:
-            with open('results/' + roundsId['name'] + '.xlsx', 'wb') as output:
+            with open(resultsFolder + '/' + roundsId['name'] + '.xlsx', 'wb') as output:
                 shutil.copyfileobj(resp.raw, output)
 
 
-def main(pastedPHPSESSID):
-    hostName = 'amsterdam.apptimizeplatform.nl'
-    baseApiUrl = 'https://' + hostName + '/api/inspectionround'
-    headers = get_login_cookies(pastedPHPSESSID)
-    roundsIds = getListofRounds(baseApiUrl, headers)
-    getXLSofRounds(roundsIds, baseApiUrl, headers)
+def main(resultsFolder):
+    hostName = 'https://amsterdam.apptimizeplatform.nl'
+    baseApiUrl = hostName + '/api/inspectionround'
+
+    with requests.Session() as session:
+        cookies = get_login_cookies(session, hostName)
+        roundsIds = getListofRounds(session, baseApiUrl, cookies)
+        getXLSofRounds(session, roundsIds, baseApiUrl, cookies, resultsFolder)
 
 
 if __name__ == '__main__':
     desc = "download all xls crow measurements"
     parser = argparse.ArgumentParser(desc)
-    parser.add_argument('phpsessid', type=str,
-                        help='paste phphsessid key here from web browser', nargs=1)
+    parser.add_argument('resultsFolder', type=str,
+                        help='Add results folder name.', nargs=1)
     args = parser.parse_args()
-    main(args.phpsessid[0])
+
+    main(args.resultsFolder[0])
